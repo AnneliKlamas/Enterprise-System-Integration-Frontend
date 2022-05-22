bc6c60da4217da90aaf8be81f5fa7178688c2927
Viido Kaur Lutsar <viidolutsar@gmail.com>
2022-05-22 22:31:50 +0300

    Prevent data from being lost with refreshing the page

--- src/stores/store-config.js
+++ src/stores/store-config.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import { authStore } from "./auth-store";
 import { productStore } from "./product-store";
+import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
     modules: {
         productStore: productStore,
         authStore: authStore
     },
-})
+    plugins: [createPersistedState()]
+});
