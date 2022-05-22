c3e9f858f6d3f3e811b709d51eb02290485defe8
Enrih Sinilaid <enrih.sinilaid@gmail.com>
2022-05-23 00:19:23 +0300

    storage change fix for header parameteres

--- src/stores/product-api.js
+++ src/stores/product-api.js
@@ -9,27 +9,21 @@ export const getProducts = () => {
 
 export const addProduct = (itemId, orderId, quantity) => {
     return Backend.post("product-entry/add", { },{
-        headers: {
-            'Authorization': 'Basic ' + localStorage.getItem('user-token')
-        },
+        headers: auth.getHeader(),
         params: { itemId: itemId, orderId: orderId, quantity: quantity }
     })
 };
 
 export const createOrder = () => {
     return Backend.post("order/create", {} ,{
-        headers: {
-            'Authorization': 'Basic ' + localStorage.getItem('user-token')
-        },
+        headers: auth.getHeader(),
         params: { address: "fake address", deliveryTime: "2007-12-03T10:15:30+01:00" }
     })
 };
 
 export const getOrder = (orderId) => {
     return Backend.get(`order/${orderId}`, {
-        headers: {
-            'Authorization': 'Basic ' + localStorage.getItem('user-token')
-        },
+        headers: auth.getHeader(),
         params: { address: "fake address", deliveryTime: "2007-12-03T10:15:30+01:00" }})
 }
 
